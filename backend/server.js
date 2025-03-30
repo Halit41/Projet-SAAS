5ef4a009050d81a027740850de01a050ede6db62
Halit41 <mhalit.soylemez@gmail.com>
2025-03-30 06:19:21 +0200

    d
    dd

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("SmartLinks API is running");
